0a0c97d2561383cbff911ab65b2fa41433189bdb
Wahyu <wahyuief@gmail.com>
2022-10-08 15:47:42 +0700

    mimo

---
+++ start.php
@@ -0,0 +1,25 @@
+<?php
+use Mimo\Core;
+use Workerman\Protocols\Http\Response;
+
+require 'vendor/autoload.php';
+
+$api = new Core('http://0.0.0.0:3000');
+
+$api->count = 4; // process count
+
+$api->any('/', function ($request) {
+    $data = json_encode(['status' => 'success', 'data' => ['status' => 'ok']]);
+    return new Response(200, ['Content-Type' => 'application/json'], $data);
+});
+
+$api->post('/hello', function ($request) {
+    $data = json_encode(['name' => $request->post('name')]);
+    return new Response(200, ['Content-Type' => 'application/json'], $data);
+});
+
+$api->post('/user/create', function ($request) {
+    return ['code'=>0 ,'message' => 'ok'];
+});
+
+$api->start();
